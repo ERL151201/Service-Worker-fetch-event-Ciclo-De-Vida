6ab1ff1aa775929bc0e261e339dcd7daea5d7c1f
Emanuel Rangel <emanuelrangelluna@gmail.com>
2022-09-30 14:59:06 -0500

    1.-Service Worker: Activate

--- sw.js
+++ sw.js
@@ -5,4 +5,14 @@ self.addEventListener('install', event => {
     //Creamos un cache
 
     console.log('SW: Instalando SW');
+
+
+});
+
+//Cuando el SW toma el control de la aplicacion
+
+self.addEventListener('activate', event => {
+
+    //Borrar cache viejo
+    console.log('SW2: Activo y listo para controlar la app');
 });
