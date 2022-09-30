45f99c3d22af1c5c9524603e8cda0648b60434f3
Emanuel Rangel <emanuelrangelluna@gmail.com>
2022-09-30 16:43:42 -0500

    3.-waituntil

--- sw.js
+++ sw.js
@@ -6,6 +6,23 @@ self.addEventListener('install', event => {
 
     console.log('SW: Instalando SW');
 
+    const instalacion = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('SW:Instalaciones terminadas');
+            self.skipWaiting();
+            resolve();
+
+
+        }, 1000);
+
+
+    });
+
+
+
+    event.waitUntil(instalacion);
+
+
 
 });
 
